79f55739bdeb33bfd49ef97f02af3b24386c4f37
Macro <macro0630@gmail.com>
2021-01-19 20:51:27 +0900

    add

--- part01/03_ex_variables.R
+++ part01/03_ex_variables.R
@@ -1,4 +1,30 @@
 # https://docs.google.com/presentation/d/1vAFR3Gut5Kc4rS_D6qTZg1oasTEoPTuXBCKgt01BJ68/edit#slide=id.g7e8cc96aa9_0_27
 
 
+a <- 11
+b <- 7
+
+Area <- a * b
+Area
+
+Perimeter <- 2 * ( a + b)
+Perimeter
+
+
+x <- 23482
+x %% 2
+
+a <- 20
+area <- 6 * a ** 2
+area
+
+volume <- a**3
+volume
+
+KMH <- 90
+MPH <- 0.6214 * KMH
+MPH
+
+
+
 
---
+++ part01/variables.R
@@ -0,0 +1,48 @@
+# 변수 : Variables
+
+fish <- '고등어'
+
+fish
+
+say <- 'hello'
+
+say
+
+price <- 3000
+
+is.married <- TRUE
+
+say
+'say'
+hello
+'hello'
+
+math_score <- 90
+
+math.score <- 80
+
+
+price * 3
+
+price * 3 + 10000
+
+price <- price * 2 + 100
+
+# 함수 : 변수처럼 생겼는데, 오른쪽에 괄호가 있다.
+
+class(fish)
+
+class(price)
+
+class(is.married)
+
+is.numeric(fish)
+
+is.character(fish)
+
+is.logical(price)
+
+is.logical(is.married)
+
+say <- 'bye'
+
